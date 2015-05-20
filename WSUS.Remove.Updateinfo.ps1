3a0025a1eed98bc5a673d5719976c1b53baeebb8
Сергей Гущин <Sergei.e.gushchin@csm.nov.ru>
2015-05-20 12:00:20 +0300

    Добавил комментарии.
    Signed-off-by: Сергей Гущин <Sergei.e.gushchin@csm.nov.ru>

--- WSUS.Remove.Updateinfo.ps1
+++ WSUS.Remove.Updateinfo.ps1
@@ -1,7 +1,13 @@
-﻿$ServerWSUS = "DNS name of your WSUS server"
+﻿# Доменное имя WSUS сервера (константа).
+$ServerWSUS = "DNS name of your WSUS server"
+# Порт WSUS сервера (константа).
 $PortNumber = "WSUS server port number"
+# Получаем сервер WSUS как объект.
 $wsus = Get-WsusServer -Name $ServerWSUS -PortNumber $PortNumber
+# Получаем объектами компьютеры с ошибками установки обновлений.
 $ComputerNameTarget = $wsus | Get-WsusComputer -IncludedInstallationStates Failed -ComputerUpdateStatus Failed
+# Из объектов берем доменные имена.
 $ComputerName = $ComputerNameTarget.FullDomainName
+# Получение на вход конвеера файлов и папок БД обновлений на найденных рабочих станциях и, соответственно, удаление.
 $Scriptblock = { Get-ChildItem -Path C:\Windows\SoftwareDistribution | Remove-Item -Recurse -Force }
 Invoke-Command -Computer $ComputerName -ScriptBlock $Scriptblock
