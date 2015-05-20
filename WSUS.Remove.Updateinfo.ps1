90918c95c896a242967ede88b7804c723254e067
Сергей Гущин <Sergei.e.gushchin@csm.nov.ru>
2015-05-20 11:46:47 +0300

    Update

---
+++ WSUS.Remove.Updateinfo.ps1
@@ -0,0 +1,7 @@
+﻿$ServerWSUS = 'srv-wsus.csm.nov.ru'
+$PortNumber = '8530'
+$wsus = Get-WsusServer -Name $ServerWSUS -PortNumber $PortNumber
+$ComputerNameTarget = $wsus | Get-WsusComputer -IncludedInstallationStates Failed -ComputerUpdateStatus Failed
+$ComputerName = $ComputerNameTarget.FullDomainName
+$Scriptblock = { Get-ChildItem -Path C:\Windows\SoftwareDistribution | Remove-Item -Recurse -Force }
+Invoke-Command -Computer $ComputerName -ScriptBlock $Scriptblock
